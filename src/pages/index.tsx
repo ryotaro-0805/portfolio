be53c6e26ab5ed1b8d0c373726fc2e9656469e11
ryotaro-0805 <rasyouryo@yahoo.co.jp>
2023-02-20 09:55:00 +0900

    🚀 作成した

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -14,110 +14,126 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
+      <header>
+        <h1 className={styles.title}>Title</h1>
+      </header>
       <main className={styles.main}>
-        <div className={styles.description}>
-          <p>
-            Get started by editing&nbsp;
-            <code className={styles.code}>src/pages/index.tsx</code>
-          </p>
-          <div>
-            <a
-              href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              By{' '}
-              <Image
-                src="/vercel.svg"
-                alt="Vercel Logo"
-                className={styles.vercelLogo}
-                width={100}
-                height={24}
-                priority
-              />
-            </a>
+        {/* 
+        websites
+         */}
+        <div className={`${styles.container} ${styles.websites_container}`}>
+          <h2>Websites</h2>
+          <div className={`${styles.wrapper} ${styles.websites_wrapper}`}>
+            <div>
+              <h3>site name</h3>
+              <h3>feature</h3>
+              <p>texttexttext</p>
+              <h3>trouble point</h3>
+              <p>texttexttext</p>
+            </div>
+            <div>
+              <h3>site name</h3>
+              <h3>feature</h3>
+              <p>texttexttext</p>
+              <h3>trouble point</h3>
+              <p>texttexttext</p>
+            </div>
+            <div>
+              <h3>site name</h3>
+              <h3>feature</h3>
+              <p>texttexttext</p>
+              <h3>trouble point</h3>
+              <p>texttexttext</p>
+            </div>
+            <div>
+              <h3>site name</h3>
+              <h3>feature</h3>
+              <p>texttexttext</p>
+              <h3>trouble point</h3>
+              <p>texttexttext</p>
+            </div>
           </div>
         </div>
 
-        <div className={styles.center}>
-          <Image
-            className={styles.logo}
-            src="/next.svg"
-            alt="Next.js Logo"
-            width={180}
-            height={37}
-            priority
-          />
-          <div className={styles.thirteen}>
-            <Image
-              src="/thirteen.svg"
-              alt="13"
-              width={40}
-              height={31}
-              priority
-            />
+        {/* 
+        applications
+         */}
+        <div className={`${styles.container} ${styles.applications_container}`}>
+          <h2>Applications</h2>
+          <div className={`${styles.wrapper} ${styles.applications_wrapper}`}>
+            <div>
+              <h3>site name</h3>
+              <h3>feature</h3>
+              <p>texttexttext</p>
+              <h3>trouble point</h3>
+              <p>texttexttext</p>
+            </div>
+            <div>
+              <h3>site name</h3>
+              <h3>feature</h3>
+              <p>texttexttext</p>
+              <h3>trouble point</h3>
+              <p>texttexttext</p>
+            </div>
+            <div>
+              <h3>site name</h3>
+              <h3>feature</h3>
+              <p>texttexttext</p>
+              <h3>trouble point</h3>
+              <p>texttexttext</p>
+            </div>
+            <div>
+              <h3>site name</h3>
+              <h3>feature</h3>
+              <p>texttexttext</p>
+              <h3>trouble point</h3>
+              <p>texttexttext</p>
+            </div>
           </div>
         </div>
 
-        <div className={styles.grid}>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2 className={inter.className}>
-              Docs <span>-&gt;</span>
-            </h2>
-            <p className={inter.className}>
-              Find in-depth information about Next.js features and&nbsp;API.
-            </p>
-          </a>
-
-          <a
-            href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2 className={inter.className}>
-              Learn <span>-&gt;</span>
-            </h2>
-            <p className={inter.className}>
-              Learn about Next.js in an interactive course with&nbsp;quizzes!
-            </p>
-          </a>
-
-          <a
-            href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2 className={inter.className}>
-              Templates <span>-&gt;</span>
-            </h2>
-            <p className={inter.className}>
-              Discover and deploy boilerplate example Next.js&nbsp;projects.
-            </p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2 className={inter.className}>
-              Deploy <span>-&gt;</span>
-            </h2>
-            <p className={inter.className}>
-              Instantly deploy your Next.js site to a shareable URL
-              with&nbsp;Vercel.
-            </p>
-          </a>
+        {/* 
+        practice
+         */}
+        <div className={`${styles.container} ${styles.practices_container}`}>
+          <h2>Practices</h2>
+          <div className={`${styles.wrapper} ${styles.practices_wrapper}`}>
+            <div>
+              <h3>site name</h3>
+              <h3>feature</h3>
+              <p>texttexttext</p>
+              <h3>trouble point</h3>
+              <p>texttexttext</p>
+            </div>
+            <div>
+              <h3>site name</h3>
+              <h3>feature</h3>
+              <p>texttexttext</p>
+              <h3>trouble point</h3>
+              <p>texttexttext</p>
+            </div>
+            <div>
+              <h3>site name</h3>
+              <h3>feature</h3>
+              <p>texttexttext</p>
+              <h3>trouble point</h3>
+              <p>texttexttext</p>
+            </div>
+            <div>
+              <h3>site name</h3>
+              <h3>feature</h3>
+              <p>texttexttext</p>
+              <h3>trouble point</h3>
+              <p>texttexttext</p>
+            </div>
+          </div>
         </div>
       </main>
+      <footer className={styles.footer}>
+        <h3>
+          This page is created by Ryotaro
+        </h3>
+      </footer>
     </>
   )
 }
