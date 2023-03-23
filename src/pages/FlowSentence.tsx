f553fe29c2b1e7e090e82084388f443a23a73d8b
ryotaro-0805 <rasyouryo@yahoo.co.jp>
2023-03-23 13:34:35 +0900

    ✅ 進めました

--- src/pages/FlowSentence.tsx
+++ src/pages/FlowSentence.tsx
@@ -7,8 +7,8 @@ const FlowSentence = () => {
     <div>
 
       <div className='flow_Sentence'>
-        <Marquee direction='left' speed={50} gradient={true} gradientColor={[243, 239, 242]}>
-        　　　　　　　　　　　　　ポートフォリオをご覧いただきありがとうございます。　ごゆっくりして行ってください。　
+        <Marquee direction='left' speed={50} gradient={true} gradientColor={[243, 239, 242]} gradientWidth={50}>
+        　　　　　　　　　　　　　ポートフォリオをご覧いただきありがとうございます。このサイトはNext.js/React-three-fiber/Swiper/React Fast Marqueeを使用して制作しております。ごゆっくりして行ってください。　
         </Marquee>
       </div>
 
