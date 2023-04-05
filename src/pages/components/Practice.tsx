2b6a7a11b3e1fecd484ee7168c94c7d2b570262b
ryotaro-0805 <rasyouryo@yahoo.co.jp>
2023-04-05 23:05:55 +0900

    ✅ 進めました

--- src/pages/components/Practice.tsx
+++ src/pages/components/Practice.tsx
@@ -1,6 +1,6 @@
 import Head from 'next/head'
 import styles from '@/styles/Home.module.css'
-import React, { useRef, useEffect} from 'react';
+import React, { useRef, useEffect } from 'react';
 
 export default function Practice() {
   // inntersection observer start
@@ -36,48 +36,60 @@ export default function Practice() {
         <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@48,400,0,0" />
       </Head>
 
-        {/* 
+      {/* 
         practice
          */}
-        <a id='practice' className={styles.jump_margin} href=""></a>
-        <div ref={addToTargets} className={`${styles.container} ${styles.appear}`}>
-          <h2>Practices</h2>
-          <p className={styles.comment}>今までにコーディングの練習として作成したものを紹介しております。</p>
-          <div className={`${styles.wrapper} ${styles.practices_wrapper}`}>
-            <div ref={addToTargets} className={`${styles.appear}`}>
-              <h3>Swiper</h3>
-              <h3>Feature</h3>
-              <p>texttexttext</p>
-              <h3>trouble point</h3>
-              <p>texttexttext</p>
-            </div>
-            <div ref={addToTargets} className={`${styles.appear}`}>
-              <h3>流れる文字</h3>
-              <h3>Feature</h3>
-              <p>texttexttext</p>
-              <h3>trouble point</h3>
-              <p>texttexttext</p>
-            </div>
-            <div ref={addToTargets} className={`${styles.appear} ${styles.practice_wrapper}`}>
-              <video autoPlay muted poster="/img/move-app-pic.png" loop playsInline>
-              <source src="/video/move-app.mp4"/>
-              </video>
+      <a id='practice' className={styles.jump_margin} href=""></a>
+      <div ref={addToTargets} className={`${styles.container} ${styles.appear}`}>
+        <h2>Practices</h2>
+        <p className={styles.comment}>今までにコーディングの練習として作成したものを紹介しております。</p>
+        <div className={`${styles.wrapper} ${styles.practices_wrapper}`}>
+          <div ref={addToTargets} className={`${styles.appear}`}>
+            <h3>Swiper</h3>
+            <h3>Feature</h3>
+            <p>texttexttext</p>
+            <h3>trouble point</h3>
+            <p>texttexttext</p>
+          </div>
+          <div ref={addToTargets} className={`${styles.appear}`}>
+            <h3>流れる文字</h3>
+            <h3>Feature</h3>
+            <p>texttexttext</p>
+            <h3>trouble point</h3>
+            <p>texttexttext</p>
+          </div>
+          {/* 自由に移動 */}
+          <div ref={addToTargets} className={`${styles.appear} ${styles.practice_wrapper}`}>
+            <video autoPlay muted poster="/img/move-app-pic.png" loop playsInline>
+              <source src="/video/move-app.mp4" />
+            </video>
+            <div className={styles.innerText}>
+            <a href="https://homepage-kankyokaihatu.vercel.app" target='_blank'>
+            </a>
               <h3>自由に移動</h3>
-              <h3>Feature</h3>
-              <p>texttexttext</p>
-              <h3>trouble point</h3>
-              <p>texttexttext</p>
-            </div>
-            <div ref={addToTargets} className={`${styles.appear}`}>
-              <h3>useful App</h3>
-              <h3>Feature</h3>
-              <p>texttexttext</p>
-              <h3>trouble point</h3>
-              <p>texttexttext</p>
+              <p className={styles.url_p}>url: https://homepage-kankyokaihatu.vercel.app/</p>
+            <hr />
+              <a href="https://github.com/ryotaro-0805/homepage-kankyokaihatu" target='_blank'>
+                <p className={styles.code_p}>code: https://github.com/ryotaro-0805/homepage-kankyokaihatu</p>
+              </a>
+              <hr />
+              <h3>Feature / 特徴</h3>
+              <p>JavaScriptで制作しております。</p>
+              <p></p>
             </div>
           </div>
+          {/* 自由に移動　ここまで */}
+
+          <div ref={addToTargets} className={`${styles.appear}`}>
+            <h3>useful App</h3>
+            <h3>Feature</h3>
+            <p>texttexttext</p>
+            <h3>trouble point</h3>
+            <p>texttexttext</p>
+          </div>
         </div>
-                {/* 
+      </div >
+      {/* 
         practiceここまで
          */}
     </>
