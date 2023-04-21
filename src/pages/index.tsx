58036ab0fed164c89157dbaa95745702711d6720
ryotaro-0805 <rasyouryo@yahoo.co.jp>
2023-04-21 11:15:24 +0900

    ✅ 進めました

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -106,34 +106,34 @@ export default function Home() {
         </div>
         <div className={styles.main_menu_wrapper}>
           <h3 className={styles.main_menu}>
-            <a style={{ color: "white" }} href="#website">
+            <a href="#website">
               Websites
             </a>
           </h3>
           <h3 className={styles.main_menu}>
-            <a style={{ color: "white" }} href="#app">
+            <a href="#app">
               Applications
             </a>
           </h3>
           <h3 className={styles.main_menu}>
-            <a style={{ color: "white" }} href="#practice">
+            <a href="#practice">
               Practices
             </a>
           </h3>
         </div>
         <div className={myCss2}>
           <h3 className={styles.appear_menu}>
-            <a style={{ color: "white" }} href="#website">
+            <a href="#website">
               Websites
             </a>
           </h3>
           <h3 className={styles.appear_menu}>
-            <a style={{ color: "white" }} href="#app">
+            <a href="#app">
               Applications
             </a>
           </h3>
           <h3 className={styles.appear_menu}>
-            <a style={{ color: "white" }} href="#practice">
+            <a href="#practice">
               Practices
             </a>
           </h3>
